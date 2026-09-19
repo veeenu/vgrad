06522faa6e733bee935017ab477525880c164cf2
Andrea Venuta <venutawebdesign@gmail.com>
2026-09-19 10:56:30 +0200

    Add loss and gradient descent

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(iter_next_chunk)]
 
+use std::iter;
 use std::mem::MaybeUninit;
 
 #[derive(Debug, Clone, Copy)]
@@ -9,6 +10,7 @@ pub struct Index(usize);
 pub enum Op {
     Sum(Index, Index),
     Mul(Index, Index),
+    Neg(Index),
     Tanh(Index),
 }
 
@@ -52,6 +54,16 @@ impl Dag {
         self.values[idx].grad = grad;
     }
 
+    pub fn get_grad(&mut self, Index(idx): Index) -> f32 {
+        self.values[idx].grad
+    }
+
+    pub fn zero_grad(&mut self) {
+        for val in &mut self.values {
+            val.grad = 0.0;
+        }
+    }
+
     pub fn add_op(&mut self, op: Op) -> Index {
         let out = self.add_value(Value::new(0.0));
         self.ops.push((op, out));
@@ -66,6 +78,9 @@ impl Dag {
                 },
                 Op::Mul(Index(a), Index(b)) => {
                     self.values[*out].datum = self.values[*a].datum * self.values[*b].datum;
+                },
+                Op::Neg(Index(i)) => {
+                    self.values[*out].datum = -self.values[*i].datum;
                 },
                 Op::Tanh(Index(i)) => {
                     self.values[*out].datum = self.values[*i].datum.tanh();
@@ -84,6 +99,9 @@ impl Dag {
                 Op::Mul(Index(a), Index(b)) => {
                     self.values[*a].grad += self.values[*b].datum * self.values[*out].grad;
                     self.values[*b].grad += self.values[*a].datum * self.values[*out].grad;
+                },
+                Op::Neg(Index(i)) => {
+                    self.values[*i].grad -= self.values[*out].grad;
                 },
                 Op::Tanh(Index(i)) => {
                     self.values[*i].grad +=
@@ -158,6 +176,10 @@ impl<const N: usize> Perceptron<N> {
     pub fn output(&self) -> Index {
         self.act
     }
+
+    fn params(&self) -> impl Iterator<Item = Index> {
+        self.w.iter().copied().chain(iter::once(self.b))
+    }
 }
 
 #[derive(Debug)]
@@ -190,7 +212,11 @@ impl<const N: usize, const M: usize> Layer<N, M> {
     }
 
     pub fn get(&mut self, dag: &mut Dag) -> [f32; M] {
-        (0..N).map(|i| dag.get_value(self.neurons[i].act)).next_chunk().unwrap()
+        (0..M).map(|i| dag.get_value(self.neurons[i].act)).next_chunk().unwrap()
+    }
+
+    fn params(&self) -> impl Iterator<Item = Index> {
+        self.neurons.iter().flat_map(Perceptron::params)
     }
 }
 
@@ -225,6 +251,15 @@ impl<const I: usize, const M: usize, const O: usize, const L: usize>
         Self { dag, input, layers, output }
     }
 
+    pub fn randomize<S: random::Source>(&mut self, mut src: S) {
+        let Self { dag, input, layers, output } = self;
+        let params =
+            input.params().chain(layers.iter().flat_map(Layer::params)).chain(output.params());
+        for param in params {
+            dag.set_value(param, (src.read_f64() * 2.0 - 1.0) as f32);
+        }
+    }
+
     pub fn eval(&mut self, x: &[f32; I]) -> [f32; O] {
         self.input.eval(&mut self.dag, x);
         self.forward();
@@ -238,6 +273,18 @@ impl<const I: usize, const M: usize, const O: usize, const L: usize>
     pub fn backward(&mut self) {
         self.dag.backward();
     }
+
+    fn learn(&mut self, lr: f32) {
+        let Self { dag, input, layers, output } = self;
+        let params =
+            input.params().chain(layers.iter().flat_map(Layer::params).chain(output.params()));
+
+        for param in params {
+            let w = dag.get_value(param);
+            let g = dag.get_grad(param);
+            dag.set_value(param, w - g * lr);
+        }
+    }
 }
 
 impl<const I: usize, const M: usize, const O: usize, const L: usize> Default
@@ -245,5 +292,64 @@ impl<const I: usize, const M: usize, const O: usize, const L: usize> Default
 {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+pub struct MeanSquareLoss<const O: usize> {
+    pred: [Index; O],
+    loss: Index,
+}
+
+impl<const O: usize> MeanSquareLoss<O> {
+    pub fn of<const I: usize, const M: usize, const L: usize>(
+        mlp: &mut MultilayerPerceptron<I, M, O, L>,
+    ) -> Self {
+        let mut pred = [Index(0); O];
+
+        for pred in &mut pred {
+            *pred = mlp.dag.add_value(Value::new(0.0));
+        }
+
+        let mut it = pred.iter().copied().zip(mlp.output.neurons.iter().map(|p| p.output()));
+        let mut loss = {
+            let (pred, out) = it.next().unwrap();
+            let pred_neg = mlp.dag.add_op(Op::Neg(pred));
+            let diff = mlp.dag.add_op(Op::Sum(pred_neg, out));
+            mlp.dag.add_op(Op::Mul(diff, diff))
+        };
+
+        for (pred, out) in it {
+            let pred_neg = mlp.dag.add_op(Op::Neg(pred));
+            let diff = mlp.dag.add_op(Op::Sum(pred_neg, out));
+            let sq_diff = mlp.dag.add_op(Op::Mul(diff, diff));
+            loss = mlp.dag.add_op(Op::Sum(loss, sq_diff));
+        }
+
+        Self { pred, loss }
+    }
+
+    pub fn apply<const I: usize, const M: usize, const L: usize>(
+        &self,
+        mlp: &mut MultilayerPerceptron<I, M, O, L>,
+        x: [f32; I],
+        y: [f32; O],
+        lr: f32,
+    ) {
+        mlp.dag.zero_grad();
+        mlp.dag.set_grad(self.loss, 1.0);
+        for (index, y) in self.pred.iter().copied().zip(y.iter().copied()) {
+            mlp.dag.set_value(index, y);
+        }
+        mlp.eval(&x);
+        mlp.dag.backward();
+
+        mlp.learn(lr);
+    }
+
+    pub fn value<const I: usize, const M: usize, const L: usize>(
+        &self,
+        mlp: &mut MultilayerPerceptron<I, M, O, L>,
+    ) -> f32 {
+        mlp.dag.get_value(self.loss)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,57 @@
+#![feature(iter_array_chunks)]
+
 use std::f32::consts::PI;
 
 use random::Source;
-use vgrad::MultilayerPerceptron;
+use vgrad::{MeanSquareLoss, MultilayerPerceptron};
 
-type MoonX<const N: usize> = [((f32, f32), (f32, f32)); N];
-type MoonY<const N: usize> = [(f32, f32); N];
+type MoonX<const N: usize> = [[f32; 2]; N];
+type MoonY<const N: usize> = [[f32; 1]; N];
 
 fn moons_dataset<const SAMPLES: usize>() -> (MoonX<SAMPLES>, MoonY<SAMPLES>) {
     let mut src = random::default(8106493);
     let mut rand = || (src.read_f64() * 0.15) as f32;
 
-    let mut x = [((0f32, 0f32), (0f32, 0f32)); SAMPLES];
+    let mut x = [[0f32; 2]; SAMPLES];
+    let mut y = [[0f32; 1]; SAMPLES];
 
-    for (i, points) in x.iter_mut().enumerate() {
-        let th = PI * (i as f32) / (SAMPLES as f32);
+    for (i, (px, py)) in
+        x.iter_mut().array_chunks::<2>().zip(y.iter_mut().array_chunks::<2>()).enumerate()
+    {
+        let th = PI * (i as f32) * 2.0 / (SAMPLES as f32);
         let x0 = th.cos() + rand();
         let y0 = th.sin() + rand();
         let x1 = 1.0 - th.cos() + rand();
         let y1 = 1.0 - th.sin() - 0.5 + rand();
-        *points = ((x0, y0), (x1, y1));
+        *px[0] = [x0, y0];
+        *px[1] = [x1, y1];
+        *py[0] = [-1.0f32];
+        *py[1] = [1.0f32];
     }
-
-    let y = [(0f32, 0f32); SAMPLES];
 
     (x, y)
 }
 
 fn main() {
-    let dataset = moons_dataset::<200>();
-    println!("{:?}", dataset);
+    let (x, y) = moons_dataset::<200>();
+    println!("{x:?}");
+    println!("{y:?}");
 
     let mut mlp: MultilayerPerceptron<2, 16, 1, 2> = MultilayerPerceptron::new();
-    println!("{:?}", mlp.eval(&[1.0, 1.0]));
-    mlp.backward();
+    mlp.randomize(random::default(31337));
+    println!("Before: n({:?}) = {:?}, {:?}", x[0], mlp.eval(&x[0]), y[0]);
+
+    let loss = MeanSquareLoss::of(&mut mlp);
+    let lr = 0.03;
+
+    for i in 0..10000 {
+        let mut total_loss = 0f32;
+        for (xs, ys) in x.iter().copied().zip(y.iter().copied()) {
+            loss.apply(&mut mlp, xs, ys, lr);
+            total_loss += loss.value(&mut mlp);
+        }
+        println!("Epoch {i:>6}. Loss: {total_loss}");
+        println!("    n({:?}) = {:?}, {:?}", x[0], mlp.eval(&x[0]), y[0]);
+        println!("    n({:?}) = {:?}, {:?}", x[1], mlp.eval(&x[1]), y[1]);
+    }
 }
